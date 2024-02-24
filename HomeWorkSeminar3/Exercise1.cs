0121d5c6570cc2a83008e147e32fa0eef13ef47b
daria <dzaytseva@mail.ru>
2024-02-24 23:16:31 +0300

    Add project files.

---
+++ HomeWorkSeminar3/Exercise1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace HomeWorkSeminar3
+{
+    internal class Exercise1
+    {
+        static public int[][] FillMatrix(int rows = 0, int columns = 0)
+        {
+            if (rows == 0 || columns == 0)
+            {
+                while (true)
+                {
+                    Console.WriteLine("Введите количество строк в матрице");
+                    var input = Console.ReadLine();
+                    if (int.TryParse(input, out rows))
+                    {
+                        Console.WriteLine("Введите количество столбцов в матрице");
+                        input = Console.ReadLine();
+                        if (int.TryParse(input, out columns))
+                            break;
+                    }
+                    Console.WriteLine("Введены неправильные значения, попробуйте еще раз");
+                }
+            }
+            Random random = new Random();
+            int[][] matrix = new int[rows][];
+            for (int i = 0; i < rows; i++)
+            {
+                matrix[i] = new int[columns];
+                for (int j = 0; j < columns; j++)
+                {
+                    matrix[i][j] = random.Next(200) - 100;
+                }
+            }
+            return matrix;
+        }
+        public void Execute()
+        {
+            Console.WriteLine("Задание 1. Случайная матрица");
+            var matrix = FillMatrix();
+            int sum = 0;
+            Console.WriteLine("Матрица: ");
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    sum += matrix[i][j];
+                    Console.Write(matrix[i][j].ToString() + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Сумма элементов матрицы: " + sum.ToString());
+        }
+    }
+}
---
+++ HomeWorkSeminar3/Exercise2.cs
@@ -0,0 +1,39 @@
+﻿namespace HomeWorkSeminar3
+{
+    internal class Exercise2
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Задание 2. Сложение матриц");
+            var matrix1 = Exercise1.FillMatrix();
+            var matrix2 = Exercise1.FillMatrix(matrix1.Length, matrix1[0].Length);
+            Console.WriteLine();
+            Console.WriteLine("Матрица 1: ");
+            for(int i = 0; i < matrix1.Length; i++)
+            {
+                for(int j = 0; j < matrix1[i].Length; j++)
+                    Console.Write(matrix1[i][j] + "\t");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("Матрица 2: ");
+            for (int i = 0; i < matrix2.Length; i++)
+            {
+                for (int j = 0; j < matrix2[i].Length; j++)
+                {
+                    matrix1[i][j] += matrix2[i][j];
+                    Console.Write(matrix2[i][j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("Сумма матриц: ");
+            for (int i = 0; i < matrix1.Length; i++)
+            {
+                for (int j = 0; j < matrix1[i].Length; j++)
+                    Console.Write(matrix1[i][j] + "\t");
+                Console.WriteLine();
+            }
+        }
+    }
+}
